1d10f9d5a159a938c534a6deac15d25cd6cee149
N1Pit0 <nikamaca5@gmail.com>
2025-05-30 20:22:57 +0400

    new module for a new microservice trainer-contribution-calculator

---
+++ TrainerContributionCalculator/src/main/java/com/mygym/crm/trainercontributioncalculator/TrainerContributionCalculatorApplication.java
@@ -0,0 +1,15 @@
+package com.mygym.crm.trainercontributioncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TrainerContributionCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerContributionCalculatorApplication.class, args);
+    }
+
+}
---
+++ TrainerContributionCalculator/src/test/java/com/mygym/crm/trainercontributioncalculator/TrainerContributionCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygym.crm.trainercontributioncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerContributionCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
