eda3dbc5f2cd29ff35f38ba02b30c23a2103e12d
N1Pit0 <nikamaca5@gmail.com>
2025-06-09 11:54:50 +0400

    Starting Implementing communications using ActiveMQ

--- TrainerContributionCalculator/src/main/java/com/mygym/crm/trainercontributioncalculator/service/MonthlySummaryService.java
+++ TrainerContributionCalculator/src/main/java/com/mygym/crm/trainercontributioncalculator/service/MonthlySummaryService.java
@@ -3,50 +3,10 @@ package com.mygym.crm.trainercontributioncalculator.service;
 import com.mygym.crm.sharedmodule.TrainerWorkloadDto;
 import com.mygym.crm.trainercontributioncalculator.domain.models.MonthlySummary;
 import com.mygym.crm.trainercontributioncalculator.domain.models.TrainerSummary;
-import com.mygym.crm.trainercontributioncalculator.service.mappers.MonthlySummaryMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class MonthlySummaryService {
-    private final MonthlySummaryMapper monthlySummaryMapper;
-    private static final Logger logger = LoggerFactory.getLogger(MonthlySummaryService.class);
+public interface MonthlySummaryService {
+    MonthlySummary findOrCreateMonthlySummary(TrainerSummary trainerSummary, TrainerWorkloadDto trainerWorkloadDto);
 
-    @Autowired
-    public MonthlySummaryService(MonthlySummaryMapper monthlySummaryMapper) {
-        this.monthlySummaryMapper = monthlySummaryMapper;
-    }
-
-    public MonthlySummary findOrCreateMonthlySummary(TrainerSummary trainerSummary, TrainerWorkloadDto trainerWorkloadDto) {
-        logger.debug("Finding or creating monthly summary for trainer: {}", trainerSummary.getUsername());
-        MonthlySummary monthlySummary = findMonthlySummary(trainerSummary, trainerWorkloadDto);
-
-        if (monthlySummary == null) {
-            monthlySummary = monthlySummaryMapper.toMonthlySummary(trainerWorkloadDto);
-            monthlySummary.setTrainerSummary(trainerSummary);
-            trainerSummary.getMonthlySummaries().add(monthlySummary);
-            logger.info("New monthly summary created for trainer: {}", trainerSummary.getUsername());
-        }
-
-        return monthlySummary;
-    }
-
-    public MonthlySummary findMonthlySummary(TrainerSummary trainerSummary, TrainerWorkloadDto trainerWorkloadDto) {
-        logger.debug("Finding monthly summary for trainer: {}", trainerSummary.getUsername());
-        MonthlySummary monthlySummary = trainerSummary.getMonthlySummaries().stream()
-                .filter(summary -> summary.getSummaryYear() == trainerWorkloadDto.getTrainingDate().getYear())
-                .filter(summary -> summary.getSummaryMonth() == monthlySummaryMapper.integerToEnum(trainerWorkloadDto.getTrainingDate().getMonthValue()))
-                .findFirst()
-                .orElse(null);
-
-        if (monthlySummary != null) {
-            logger.debug("Monthly summary found for trainer: {}", trainerSummary.getUsername());
-        } else {
-            logger.debug("Monthly summary not found for trainer: {}", trainerSummary.getUsername());
-        }
-
-        return monthlySummary;
-    }
+    MonthlySummary findMonthlySummary(TrainerSummary trainerSummary, TrainerWorkloadDto trainerWorkloadDto);
 }
+
--- TrainerContributionCalculator/src/main/java/com/mygym/crm/trainercontributioncalculator/service/TrainerSummaryService.java
+++ TrainerContributionCalculator/src/main/java/com/mygym/crm/trainercontributioncalculator/service/TrainerSummaryService.java
@@ -2,52 +2,12 @@ package com.mygym.crm.trainercontributioncalculator.service;
 
 import com.mygym.crm.sharedmodule.TrainerWorkloadDto;
 import com.mygym.crm.trainercontributioncalculator.domain.models.TrainerSummary;
-import com.mygym.crm.trainercontributioncalculator.repositories.TrainingRepository;
-import com.mygym.crm.trainercontributioncalculator.service.mappers.TrainerSummaryMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class TrainerSummaryService {
-    private static final Logger logger = LoggerFactory.getLogger(TrainerSummaryService.class);
+public interface TrainerSummaryService {
+    TrainerSummary createTrainerSummary(TrainerWorkloadDto trainerWorkloadDto);
 
-    private final TrainingRepository trainingRepository;
-    private final TrainerSummaryMapper trainerSummaryMapper;
+    TrainerSummary updateTrainerSummary(TrainerSummary trainerSummary);
 
-    @Autowired
-    public TrainerSummaryService(TrainingRepository trainingRepository, TrainerSummaryMapper trainerSummaryMapper) {
-        this.trainingRepository = trainingRepository;
-        this.trainerSummaryMapper = trainerSummaryMapper;
-    }
+    TrainerSummary findByUsername(String username);
 
-    @Transactional
-    public TrainerSummary createTrainerSummary(TrainerWorkloadDto trainerWorkloadDto) {
-        logger.debug("Creating trainer summary for trainer: {}", trainerWorkloadDto.getUserName());
-        TrainerSummary trainerSummary = trainerSummaryMapper.toTrainerSummary(trainerWorkloadDto);
-        TrainerSummary savedTrainerSummary = trainingRepository.save(trainerSummary);
-        logger.info("Trainer summary created for trainer: {}", savedTrainerSummary.getUsername());
-        return savedTrainerSummary;
-    }
-
-    @Transactional
-    public TrainerSummary updateTrainerSummary(TrainerSummary trainerSummary) {
-        logger.debug("Updating trainer summary for trainer: {}", trainerSummary.getUsername());
-        TrainerSummary updatedTrainerSummary = trainingRepository.save(trainerSummary);
-        logger.info("Trainer summary updated for trainer: {}", updatedTrainerSummary.getUsername());
-        return updatedTrainerSummary;
-    }
-
-    public TrainerSummary findByUsername(String username) {
-        logger.debug("Finding trainer summary by username: {}", username);
-        TrainerSummary trainerSummary = trainingRepository.findByUsername(username);
-        if (trainerSummary != null) {
-            logger.debug("Trainer summary found for username: {}", username);
-        } else {
-            logger.debug("Trainer summary not found for username: {}", username);
-        }
-        return trainerSummary;
-    }
 }
